a7d15aa7125cf3a27433d79d7265d9f261ca5f9b
Guide Fari <guide.fari@flexperto.com>
2023-06-26 18:23:55 +0200

    refactor: tmuxifier layouts tracked separately

---
+++ tmuxifier/.tmux/plugins/tmuxifier/layouts/config.session.sh
@@ -0,0 +1,17 @@
+# Set a custom session root path. Default is `$HOME`.
+# Must be called before `initialize_session`.
+session_root "~/dotfiles/"
+
+# Create session with specified name if it does not already exist. If no
+# argument is given, session name will be based on layout file name.
+if initialize_session "config"; then
+
+  # Create a new window inline within session layout definition.
+  new_window "config"
+  run_cmd "nvim ."
+
+fi
+
+# Finalize session creation and switch/attach to it.
+finalize_and_go_to_session
+
---
+++ tmuxifier/.tmux/plugins/tmuxifier/layouts/here_hugo.session.sh
@@ -0,0 +1,20 @@
+# Set a custom session root path. Default is `$HOME`.
+# Must be called before `initialize_session`.
+session_root "~/source/here_hugo/"
+
+# Create session with specified name if it does not already exist. If no
+# argument is given, session name will be based on layout file name.
+if initialize_session "here_hugo"; then
+
+  # Create a new window inline within session layout definition.
+  new_window "here_hugo"
+  split_v 20
+  run_cmd "yarn dev"
+  split_h 50
+  select_pane 0
+  run_cmd "nvim ."
+
+fi
+
+# Finalize session creation and switch/attach to it.
+finalize_and_go_to_session
