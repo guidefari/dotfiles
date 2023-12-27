f2473924db4e8f8ee44c3d46e5e79f6a6d8b1a7a
Guide Fari <guideg6@gmail.com>
2023-12-27 06:08:08 +0200

    tmux: effect

--- tmuxifier/.tmux/plugins/tmuxifier/layouts/effect.session.sh
+++ tmuxifier/.tmux/plugins/tmuxifier/layouts/effect.session.sh
@@ -1,15 +1,12 @@
 # Set a custom session root path. Default is `$HOME`.
 # Must be called before `initialize_session`.
-session_root "~/source/spotify-effect/"
+session_root "~/source/effect/"
 
 # Create session with specified name if it does not already exist. If no
 # argument is given, session name will be based on layout file name.
-if initialize_session "spotify-effect"; then
+if initialize_session "effect"; then
 
   # Create a new window inline within session layout definition.
-  new_window "server"
-  run_cmd "yarn"
-  run_cmd "yarn dev"
   new_window "nvim"
   run_cmd "nvim ."
 
